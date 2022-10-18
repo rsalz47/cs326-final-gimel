64e5a3692badabfc739dfe552658302443a85955
rsalz47 <ronan.salz@gmail.com>
2022-10-18 18:59:22 -0400

    added third table on RHS of home page

--- src/frontend/graph.js
+++ src/frontend/graph.js
@@ -9,10 +9,10 @@ const chartData = {
 };
 
 const covg = document.getElementById("coverage-graph");
-console.log("chLine parent element width is: ", covg.parentElement.clientWidth);
+console.log("covg parent element width is: ", covg.parentElement.clientWidth);
 if (covg) {
     // eslint-disable-next-line no-new, no-undef
-    new Chart(chLine, {
+    new Chart(covg, {
         type: "line",
         data: chartData,
         options: {
@@ -29,3 +29,7 @@ if (covg) {
         }
     });
 }
+
+// <canvas id="coverage-graph"></canvas>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+// <script id="graph" src="src/frontend/graph.js" defer></script>
