4b3f32e0c556c5c23bcec88baa28a1f03c2d8ca6
rsalz47 <ronan.salz@gmail.com>
2022-10-18 21:08:44 -0400

    added graph drawing capabilities

--- src/frontend/graph.js
+++ src/frontend/graph.js
@@ -1,35 +1,71 @@
-const chartData = {
-    labels: ["S", "M", "T", "W", "T", "F", "S"],
-    datasets: [{
-        data: [589, 445, 483, 503, 689, 692, 634],
-    },
-    {
-        data: [639, 465, 493, 478, 589, 632, 674],
+// Various variables
+
+// First checkbox on LHS of graph page, used to show/hide graph1
+const toggle1 = document.getElementById("toggle1");
+
+// Second checkbox on LHS of graph page, used to un/set logarithmic scale
+const toggle2 = document.getElementById("toggle2");
+
+// Logarithmic scale for graph drawing
+const logarithmicScale = {myScale: {
+    type: "logarithmic",
+    position: "right"
+}};
+// Default scale for graph drawing
+const defaultScale = {
+    yAxes: [{
+        ticks: {
+            beginAtZero: false
+        }
     }]
 };
 
-const covg = document.getElementById("coverage-graph");
-console.log("covg parent element width is: ", covg.parentElement.clientWidth);
-if (covg) {
-    // eslint-disable-next-line no-new, no-undef
-    new Chart(covg, {
-        type: "line",
-        data: chartData,
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: false
-                    }
-                }]
-            },
-            legend: {
-                display: false
-            }
-        }
-    });
-}
+// Globals used in graph drawing
+let chart1 = null;
+let currScale = defaultScale;
 
-// <canvas id="coverage-graph"></canvas>
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-// <script id="graph" src="src/frontend/graph.js" defer></script>
+// Dummy data to draw graph 1, later on this will be made dynamic with database queries
+const lineData = {
+    labels: [0, 10, 20, 30, 40, 50],
+    datasets: [{
+        label: "My First Dataset",
+        data: [0, 59, 80, 81, 100, 150, 156],
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.5
+    }]
+};
+
+/* eslint-disable prefer-arrow-callback */
+// const leftPane = document.getElementById("view1");
+
+// EventListeners()
+
+// draw or delete graph1
+toggle1.addEventListener("change", function () {
+    const covg = document.getElementById("coverage-graph");
+
+    if (toggle1.checked) {
+        // eslint-disable-next-line no-undef
+        chart1 = new Chart(covg, {
+            type: "line",
+            data: lineData,
+            options: {
+                scales: defaultScale,
+                legend: {
+                    display: false
+                }
+            }
+        });
+    } else if (chart1 !== null) {
+        console.log("destroying chart");
+        chart1.destroy();
+        chart1 = null;
+    }
+});
+
+// Un/set scale to logarithmic
+toggle2.addEventListener("change", function () {
+    currScale = (toggle2.checked ? logarithmicScale : defaultScale);
+    console.log(currScale);
+});
--- src/frontend/notes.js
+++ src/frontend/notes.js
@@ -1,10 +1,10 @@
 /// Insert a note into the notes listing
-function insert_note(user, text){
+function insert_note(user, text) {
     const ts = new Date();
     const notes = document.getElementById("notes");
     const li = document.createElement("li");
 
-    li.classList.add('note');
+    li.classList.add("note");
 
     li.appendChild(document.createTextNode(ts.toLocaleDateString() + " : "));
     li.appendChild(document.createTextNode(ts.toLocaleTimeString()));
@@ -15,16 +15,16 @@ function insert_note(user, text){
     notes.appendChild(li);
 }
 
-/// Read the cur_note field and save the note to the database-notes listing before clearing the 
+/// Read the cur_note field and save the note to the database-notes listing before clearing the
 /// field
 function send_note() {
-    const msg = document.getElementById('cur_note').value;
+    const msg = document.getElementById("cur_note").value;
     insert_note("sample_user", msg);
-    document.getElementById('cur_note').value = "";
+    document.getElementById("cur_note").value = "";
 }
 
 /// Register event listener to send the note when the enter key is pressed
-document.getElementById('cur_note').addEventListener('keydown', (event) => {
+document.getElementById("cur_note").addEventListener("keydown", event => {
     if (event.keyCode === 13) {
         send_note();
     }
