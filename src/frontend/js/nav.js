29e482b5f52fb7d726df6ae23240604272694410
seal9055 <seal9055@gmail.com>
2022-10-21 17:00:58 -0400

    project restructuring

---
+++ src/frontend/js/nav.js
@@ -0,0 +1,10 @@
+function insert_nav(path) {
+    fetch(path + 'nav.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#navbar");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem,oldelem);
+    })
+}
--- src/frontend/nav.js
+++
@@ -1,8 +0,0 @@
-fetch('src/frontend/nav.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
