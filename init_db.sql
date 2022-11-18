2de6e01c70ec62223610442d27b7e83a4626b53b
rsalz47 <61328965+rsalz47@users.noreply.github.com>
2022-11-18 17:45:52 -0500

    Add files via upload

---
+++ init_db.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA fizzy;
+CREATE TABLE fizzy.users(id SERIAL PRIMARY KEY, name varchar(255), role varchar(1), handle varchar(255));
+CREATE TABLE fizzy.comments(id integer, comment text, timestamp text, name varchar(255));
+CREATE TABLE fizzy.stats(cases_total bigint, crash_total bigint, 
+			crash_unique bigint, run_time text, coverage bigint, 
+			cmp_cov bigint, instr_count bigint, timeouts bigint);
+CREATE TABLE fizzy.project(name text, fuzzer text, target text, input_dir text, 
+			   output_dir text, timestamp text);
