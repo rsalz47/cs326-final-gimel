43b8c671014fad8218e457c1361515711bc18bab
rsalz47 <61328965+rsalz47@users.noreply.github.com>
2022-11-18 18:08:15 -0500

    db initialization script

--- init_db.sql
+++ init_db.sql
@@ -1,8 +1,13 @@
+DROP SCHEMA fizzy CASCADE;
+
 CREATE SCHEMA fizzy;
 CREATE TABLE fizzy.users(id SERIAL PRIMARY KEY, name varchar(255), role varchar(1), handle varchar(255));
 CREATE TABLE fizzy.comments(id integer, comment text, timestamp text, name varchar(255));
 CREATE TABLE fizzy.stats(cases_total bigint, crash_total bigint, 
-			crash_unique bigint, run_time text, coverage bigint, 
+			crash_unique bigint, run_time bigint, coverage bigint, 
 			cmp_cov bigint, instr_count bigint, timeouts bigint);
 CREATE TABLE fizzy.project(name text, fuzzer text, target text, input_dir text, 
 			   output_dir text, timestamp text);
+
+INSERT INTO fizzy.stats
+VALUES (0, 0, 0, 0, 0);
