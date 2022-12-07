dfe514be247787bcb6369132df026417dc7dda6f
Dung Nguyen <dung@umass.edu>
2022-12-06 22:45:16 -0500

    fix: in-line js

--- src/frontend/js/nav.js
+++ src/frontend/js/nav.js
@@ -1,11 +1,11 @@
 /// Inserts navbat into html file when called
-function insert_nav(path) {
-    fetch(path + 'nav.html')
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector("script#navbar");
-        let newelem = document.createElement("div");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem,oldelem);
-    })
-}
+(function insert_nav(path) {
+    fetch(path + "nav.html")
+        .then(res => res.text())
+        .then(text => {
+            const oldelem = document.querySelector("script#navbar");
+            const newelem = document.createElement("div");
+            newelem.innerHTML = text;
+            oldelem.parentNode.replaceChild(newelem, oldelem);
+        });
+})("");
